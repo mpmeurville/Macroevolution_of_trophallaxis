5d17ca7ed992687e3a9a33cedfd15a307c8ed307
mpmeurville <33485790+mpmeurville@users.noreply.github.com>
2025-01-26 18:17:25 +0100

    Update launch.sh

--- 02_BNN/run_5_5/launch.sh
+++ 02_BNN/run_5_5/launch.sh
@@ -5,10 +5,6 @@ parent_dir="/media/meurvill/Elements/re-do_V2/02_BNN/run_5_5"
 # Loop over child folders CV0 to CV9
 for i in {0..9}; do
     child_dir="${parent_dir}/CV${i}"
-
-    # Open a new terminal tab and execute the Python script
-    
-	#echo $child_dir
     gnome-terminal --tab --working-directory="${child_dir}" --title="CV${i}" -- sh -c "python3 'bnn_classify.py' ; bash"
 done
 
